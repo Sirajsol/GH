510862b29f5e89b286dbbfeb466dc8a0796ba711
Siraj soliman <healthtartus@gmail.com>
2024-09-19 21:12:12 +0300

    firtt commit

--- src/singleitem/SingleItem.jsx
+++ src/singleitem/SingleItem.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ItemInfo from '../itemInfo/itemInfo'
 import Carousal from '../caraousel/Carousal'
-// import './Singleitem.css'
+import './SingleItem.css'
 function SingleItem() {
   return (
     <div className='single_whole'>
